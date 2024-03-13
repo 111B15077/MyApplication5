f747c69100423cf286fbb0fd59b6984e17f76656
yum <111B15077@mailst.cjcu.edu.tw>
2024-03-13 15:51:18 +0800

    3/11 3:00

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void calcBMI(View view) {
+        EditText height = findViewById(R.id.etHeight);
+        EditText weight = findViewById(R.id.etweight);
+        double h = Double.parseDouble(height.getText().toString());
+        double w = Double.parseDouble(weight.getText().toString());
+        double bmi = w / (h/100.0 * h/100.0);
+        String result = "你的BMI是 " + bmi;
+        TextView show = findViewById(R.id.tvshow);
+        show.setText(result);
     }
 }
