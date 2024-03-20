efc0fd8f0c6dbea92e79ce7fd30302960b216d35
yum <111B15077@mailst.cjcu.edu.tw>
2024-03-20 15:51:27 +0800

    製作增加多國語系

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,15 +19,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void calcBMI(View view) {
+        DecimalFormat df = new DecimalFormat("0.00");
         EditText height = findViewById(R.id.etHeight);
         EditText weight = findViewById(R.id.etweight);
         double h = Double.parseDouble(height.getText().toString());
         double w = Double.parseDouble(weight.getText().toString());
 
-        double bmi = w / (h/100.0 * h/100.0);
-        String result = "你的BMI是 " + bmi;
-
+        double bmi = w / (h / 100.0 * h / 100.0);
+        bmi = Double.parseDouble(df.format(bmi));
+        String result = getString(R.string.strshowbmi) + bmi;
         TextView show = findViewById(R.id.tvshow);
         show.setText(result);
+    }
+
+    public void goNext(View view) {
+        Intent intent = new Intent(this, ResulActivity.class);
+        startActivity(intent);
     }
 }
