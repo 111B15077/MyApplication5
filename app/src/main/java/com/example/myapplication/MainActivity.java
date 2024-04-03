aa710815a5be30699a24cd32158a33976f98670a
yum <111B15077@mailst.cjcu.edu.tw>
2024-04-03 15:37:10 +0800

    4/3 2:19 男女性別

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
@@ -15,13 +18,30 @@ public class MainActivity extends AppCompatActivity {
     private TextView show;
     private EditText height;
     private EditText weight;
+    private RadioGroup rbsex;
+    private CheckBox cbapple;
+    private CheckBox cbbanana;
+    private CheckBox cborange;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViews();
+
+        rbsex.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.rbMale) {
+                    show.setText("我是男生");
+                } else if (checkedId == R.id.rbFemale) {
+                    show.setText("我是女生");
+                }
+            }
+        });
+
     }
+
 
     public void calcBMI(View view) {
         DecimalFormat df = new DecimalFormat("#.00");
@@ -51,46 +71,7 @@ public class MainActivity extends AppCompatActivity {
         height = findViewById(R.id.etHeight);
         weight = findViewById(R.id.etweight);
         show = findViewById(R.id.tvshow);
+        rbsex = findViewById(R.id.rbsex);
     }
+
 }
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//    }
-//
-//    public void calcBMI(View view) {
-//        DecimalFormat df = new DecimalFormat("0.00");
-//        double bmi = getBmi();
-//        String result = getString(R.string.strshowbmi) + bmi;
-//        show.setText(result);
-//    }
-//
-//    private double getBmi() {
-//        DecimalFormat df = new DecimalFormat("0.00");
-//        double h = Double.parseDouble(height.getText().toString());
-//        double w = Double.parseDouble(weight.getText().toString());
-//        double bmi = w / (h / 100.0 * h / 100.0);
-//        bmi = Double.parseDouble(df.format(bmi));
-//        return bmi;
-//    }
-//
-//
-//
-//    public void goNext(View view) {
-//        Intent intent = new Intent(this, ResulActivity.class);
-//        double bmi = getBmi();
-//        intent.putExtra("bmi", bmi);
-//        startActivity(intent);
-//
-//    }
-//
-//    private void findViews() {
-//        height = findViewById(R.id.etHeight);
-//        weight = findViewById(R.id.etweight);
-//        show = findViewById(R.id.tvshow);
-//    }
-//}
-
-
