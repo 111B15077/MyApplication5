2e4e0cf890a49ff994c35ec924ebdbda6acea378
yum <111b15077@mailst.cjcu.edu.tw>
2024-04-24 15:14:14 +0800

    4/24 3:14

--- app/src/main/java/com/example/myapplication/ListActivity.java
+++ app/src/main/java/com/example/myapplication/ListActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -26,17 +27,45 @@ public class ListActivity extends AppCompatActivity {
         list.add("香蕉");
         list.add("橘子");
         list.add("西瓜");
+        list.add("芭樂");
+        list.add("梨子");
+        list.add("葡萄");
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, list);
+
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner SpFrulit = findViewById(R.id.SpFrulit);
         TextView tvshow1 = findViewById(R.id.tvshow1);
-        SpFrulit.setAdapter(adapter);
+        ListView lvfruit = findViewById(R.id.lvFruit);
+
+        lvfruit.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        lvfruit.setAdapter(adapter);
+//        SpFrulit.setAdapter(adapter);
+        lvfruit.setAdapter(adapter);
 
         SpFrulit.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 tvshow1.setText("你選擇了" + parent.getSelectedItem().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                tvshow1.setText("請選擇水果");
+            }
+        });
+
+        lvfruit.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                tvshow1.setText(list.get(position));
+            }
+        });
+
+        lvfruit.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                tvshow1.setText(list.get(position));
             }
 
             @Override
