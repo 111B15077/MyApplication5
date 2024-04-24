67ddddff7a27a2c9f38f34b5e0b15bbff8ba4c05
yum <111B15077@mailst..cjcu.edu.tw>
2024-04-24 14:21:24 +0800

    4/24 2:24

--- app/src/main/java/com/example/myapplication/ListActivity.java
+++ app/src/main/java/com/example/myapplication/ListActivity.java
@@ -3,15 +3,18 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class ListActivity extends AppCompatActivity {
-    private Spinner SpFrulit;
+//    private Spinner SpFrulit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +25,24 @@ public class ListActivity extends AppCompatActivity {
         list.add("蘋果");
         list.add("香蕉");
         list.add("橘子");
+        list.add("西瓜");
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, list);
-
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner SpFrulit = findViewById(R.id.SpFrulit);
+        TextView tvshow1 = findViewById(R.id.tvshow1);
+        SpFrulit.setAdapter(adapter);
+
+        SpFrulit.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                tvshow1.setText("你選擇了" + parent.getSelectedItem().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                tvshow1.setText("請選擇水果");
+            }
+        });
     }
 }
